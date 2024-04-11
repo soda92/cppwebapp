d02f0c565fe3ca0695abc5ec1d99e2efb57be5ba
soda <soda.local>
2024-04-11 14:47:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Env setup
+
+## Windows
+VS2022(C++ desktop), scoop(cmake, ninja), LLVM, VSCode(clangd, C/C++, CMake Tools)
---
+++ main.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <boost/format.hpp>
+
+int main(){
+    std::cout << boost::format("%1%, %2%!") % "Hello" % "World";
+}
